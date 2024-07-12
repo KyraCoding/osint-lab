eaaceaa5b87ae17cef89975af7ea3da7121dbd1b
Glitch (osint-academy) <none>
2024-07-12 21:44:30 +0000

    🛩️🕟 Updated with Glitch

--- views/assets/practice.js
+++ views/assets/practice.js
@@ -1,21 +1,60 @@
-console.log("active")
 function overlay(element) {
-  document.getElementById("overlay").classList.remove("hidden")
-  document.getElementById("overlay").classList.add("animate-fadein")
-  document.body.classList.add("overflow-hidden")
+  document.getElementById("overlay").classList.remove("hidden");
+  document.getElementById("overlay").classList.add("animate-fadein");
+  document.body.classList.add("overflow-hidden");
   document.getElementById("challenge_div").scrollTop = 0;
-}
-document.getElementById('challenge_div').addEventListener('click', function(event) {
-  event.stopPropagation();
-});
 
-document.getElementById("overlay").addEventListener('click', function() {
-  document.getElementById("overlay").classList.remove("animate-fadein")
+  const data = JSON.parse(element.dataset.raw_data);
+  Object.keys(data).forEach((attr) => {
+    if (document.getElementById("challenge_div_" + attr)) {
+      if (attr == "author") {
+        data[attr] = "Author: " + data[attr];
+      }
+      document.getElementById("challenge_div_" + attr).innerHTML = data[attr];
+    }
+    var color = "bg-[#37E100]";
+    switch (data.difficulty) {
+      case "beginner":
+        color = "bg-[#00A7E1]";
+        break;
+      case "easy":
+        color = "bg-[#37E100]";
+        break;
+      case "medium":
+        color = "bg-[#E13A00]";
+        break;
+      case "hard":
+        color = "bg-[#AA00E1]";
+        break;
+      default:
+        break;
+    }
+    document
+      .getElementById("challenge_div_difficulty")
+      .classList.remove(
+        document.getElementById("challenge_div_difficulty").dataset
+          .current_color
+      );
+    document.getElementById("challenge_div_difficulty").classList.add(color);
+    document.getElementById("challenge_div_difficulty").dataset.current_color =
+      color;
+  });
+}
+document
+  .getElementById("challenge_div")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+
+document.getElementById("overlay").addEventListener("click", function () {
+  document.getElementById("overlay").classList.remove("animate-fadein");
   document.getElementById("overlay").classList.add("hidden");
-  document.body.classList.remove("overflow-hidden")
-})
-document.getElementById("challenge_div_close").addEventListener('click', function() {
-  document.getElementById("overlay").classList.remove("animate-fadein")
-  document.getElementById("overlay").classList.add("hidden");
-  document.body.classList.remove("overflow-hidden")
-})
+  document.body.classList.remove("overflow-hidden");
+});
+document
+  .getElementById("challenge_div_close")
+  .addEventListener("click", function () {
+    document.getElementById("overlay").classList.remove("animate-fadein");
+    document.getElementById("overlay").classList.add("hidden");
+    document.body.classList.remove("overflow-hidden");
+  });
