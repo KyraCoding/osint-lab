532e3dde82db3c72f3d24fc23f282b826436db5c
Glitch (osint-academy) <none>
2024-07-12 19:11:12 +0000

    💸🖤 Updated with Glitch

--- views/assets/practice.js
+++ views/assets/practice.js
@@ -2,6 +2,7 @@ console.log("active")
 function overlay(element) {
   document.getElementById("overlay").classList.remove("hidden")
   document.getElementById("overlay").classList.add("animate-fadein")
+  document.body.classList.add("overflow-hidden")
 }
 document.getElementById('challenge_div').addEventListener('click', function(event) {
   event.stopPropagation();
@@ -10,4 +11,10 @@ document.getElementById('challenge_div').addEventListener('click', function(even
 document.getElementById("overlay").addEventListener('click', function() {
   document.getElementById("overlay").classList.remove("animate-fadein")
   document.getElementById("overlay").classList.add("hidden");
+  document.body.classList.remove("overflow-hidden")
+})
+document.getElementById("challenge_div_close").addEventListener('click', function() {
+  document.getElementById("overlay").classList.remove("animate-fadein")
+  document.getElementById("overlay").classList.add("hidden");
+  document.body.classList.remove("overflow-hidden")
 })
